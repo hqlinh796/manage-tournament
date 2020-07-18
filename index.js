c6634d8f36cc3b8d5a6e2047820b2384736e2c42
Võ Văn Lình <vovanlinhleo1999@gmail.com>
2020-07-18 23:21:40 +0700

    update UI

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+//doi bong
+const doiBongRouter = require('./routes/doi_bong');
+
 const {sequelize} = require('./models/');
 const Umzug = require('umzug');
 const db = require('./models');
@@ -49,6 +52,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/doibong', doiBongRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
